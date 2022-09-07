54d01b1f1881ae670220a313ff2bb6b5c28e43a8
lnicosia <lnicosia@student.42.fr>
2022-09-07 12:44:50 +0200

    [ADD] level1 ok.

--- level1/level1.c
+++ level1/level1.c
@@ -1,13 +1,13 @@
 void	run(void)
 {
-	fwrite("Good... Wait what?\n", 1, 0x13, stdout);
+	fwrite("Good... Wait what?\n", 1, 19, stdout);
 	system("/bin/sh");
 	return;
 }
 
 int		main()
 {
-	char	buf[76];
+	char	buf[72];
 
 	gets(buf);
 	return 0;
